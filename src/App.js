61d3b0e59244302ccbed5ba8906cf94c5dbd3a04
Donikena Dharani <124602731+Donikenadharani@users.noreply.github.com>
2025-04-17 21:50:41 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   const [students, setStudents] = useState([]);
 
   useEffect(() => {
-    fetch('assignmentbackend-dtgmctfqavbubrck.centralus-01.azurewebsites.net/api/students')
+    fetch('https://assignmentbackend-dtgmctfqavbubrck.centralus-01.azurewebsites.net/api/students')
       .then(res => res.json())
       .then(data => setStudents(data))
       .catch(err => console.error('Error fetching students:', err));
